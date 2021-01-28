7078179b5d4d666358b227a9d719fbca4bf0d879
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2021-01-28 15:56:49 +0100

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+if(!localStorage.getItem('token')) {
+  fetch('https://api-slack-token-nlc.herokuapp.com/')
+  .then(response => response.json())
+  .then(data => {
+    localStorage.setItem('token', data.token);
+  });
+}
+
+document.getElementById('submit').addEventListener('click',() => {
+
+  let email = document.getElementById('email').value;
+  let message = document.getElementById('message').value;
+  let txt = `Veuillez répondre à ${email}\n${message}`;
+
+  let auth = `Bearer ${localStorage.getItem('token')}`;
+
+  fetch('https://slack.com/api/chat.postMessage', { 
+    method: 'POST', 
+    headers: new Headers({
+      'Content-type': 'application/json',
+      'Authorization': auth
+    }), 
+    body:  JSON.stringify({channel:"C01L220T2TF",text: txt})
+  });
+})
